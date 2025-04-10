69e930e8839a5c140547660a2c440c86d3f6a30d
Islomjonsdev <islomjonmukhammadaliev4@gmail.com>
2025-04-10 14:44:44 +0500

    header done

--- components/shared/header.jsx
+++ components/shared/header.jsx
@@ -1,16 +1,64 @@
-import React from 'react';
-import sleepNestLogo from "../../public/images/sleepnest_logo.png"
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import sleepNestLogo from "../../public/images/sleepnest_logo.png";
+import { FcLike } from "react-icons/fc";
+import { Menu } from "lucide-react";
 
 const Header = () => {
+  const navLinks = [
+    {
+      to: "/",
+      label: "Home",
+    },
+    {
+      to: "/collection",
+      label: "Collection",
+    },
+    {
+      to: "/aboutus",
+      label: "About Us",
+    },
+    {
+      to: "/contacts",
+      label: "Contacts",
+    },
+  ];
   return (
-    <header className='bg-[red]'>
-        <div className='max-w-[1400px] w-full mx-auto px-[20px]'>
-            <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, magnam quaerat dicta voluptate aperiam magni quae. Modi, reiciendis ducimus. Rerum earum magni blanditiis cumque error illum nulla odit et fugiat. Quisquam, veritatis. Facilis et aliquid atque pariatur voluptates, nesciunt non quidem cumque doloremque labore harum in reiciendis velit blanditiis. Autem minus dolorem ullam. Laboriosam dicta quidem, facere delectus incidunt ab, vel deserunt, magnam voluptatum est reiciendis! Consequuntur nemo earum perferendis, accusantium doloremque, reiciendis sapiente odit iusto harum aperiam porro nihil deleniti veritatis commodi quam, ipsa ipsum quod minus quasi maiores quo eius rerum! Sequi voluptatem ea, commodi odit corporis dignissimos.
-            </div>
-        </div>
-    </header>
-  )
-}
+    <header className="bg-white py-[10px] bg-white py-[10px] shadow-md">
+      <div className="max-w-[1400px] w-full mx-auto px-[20px]">
+        <div className="flex items-center justify-between">
+          <Link href={"/"}>
+            <Image
+              src={sleepNestLogo}
+              alt="sleepNestLogo"
+              width={180}
+              height={55}
+            />
+          </Link>
+          <div className="flex items-center gap-[15px]">
+            <ul className="hidden lg:flex items-center gap-[30px] my-[16px]">
+              {navLinks?.map(({ to, label }) => (
+                <li className="py-[5px] px-[8px]" key={to}>
+                  <Link href={to}>{label}</Link>
+                </li>
+              ))}
+            </ul>
+            <button className="hidden lg:block">
+              <FcLike className="w-[20px] h-[20px]" />
+            </button>
+            <select name="" id="">
+              <option value=""></option>
+            </select>
 
-export default Header
+            <button className="lg:hidden block">
+              <Menu />
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
