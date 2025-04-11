082e98a2c6374ec06720807cb37adfeb233e44c9
Islomjonsdev <islomjonmukhammadaliev4@gmail.com>
2025-04-11 23:31:31 +0500

    hero started

--- app/(root)/(home)/page.jsx
+++ app/(root)/(home)/page.jsx
@@ -1,8 +1,11 @@
+import Hero from '@/components/shared/hero'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Main</div>
+    <>
+      <Hero />
+    </>
   )
 }
 
---
+++ components/shared/hero.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import React from "react";
+import heroImage from "../../public/images/hero.png";
+
+const Hero = () => {
+  return (
+    <section>
+      <div className="max-w-[1400px] w-full mx-auto px-[20px] ">
+        <div className="bg-[red]">
+        salom
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
