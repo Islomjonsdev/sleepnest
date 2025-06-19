f45442b819b78580fce5d714cde6634a58a51e87
Islomjonsdev <islomjonmukhammadaliev4@gmail.com>
2025-06-20 01:46:19 +0500

    in collection page structure done

--- app/(root)/(home)/collection/page.jsx
+++ app/(root)/(home)/collection/page.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Collection = () => {
   return (
-    <div className='mt-[50px]'>Collection</div>
+    <div className='mt-[50px]'>
+      <div className='max-w-[1440px] mx-auto px-[20px]'>
+          <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4'>
+              <div className='bg-[red] md:col-span-1'>salom</div>
+              <div className='bg-[blue] md:col-span-2'>salom</div>
+          </div>
+      </div>
+    </div>
   )
 }
 
