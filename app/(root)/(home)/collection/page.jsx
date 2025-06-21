f9deb623ca734747e72a42ec5c7bc6afb3995021
Islomjonsdev <islomjonmukhammadaliev4@gmail.com>
2025-06-21 13:23:43 +0500

    done

--- app/(root)/(home)/collection/page.jsx
+++ app/(root)/(home)/collection/page.jsx
@@ -61,7 +61,7 @@ const Collection = () => {
               ))}
             </div>
           </div>
-          <div className="md:col-span-2 grid grid-cols-4 gap-4">
+          <div className="md:col-span-2 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {searchByTitle?.map(({ id, img, title, category }) => (
               <Link key={id} href={"/"}>
                 <CollectionCard img={img} title={title} category={category} />
